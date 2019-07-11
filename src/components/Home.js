7a9bdb4a3f6f87e791d0241d38616a24c4a4e81b
TuckerNemcek <tuckernemcek21@gmail.com>
2019-07-11 10:26:40 -0600

    added react router, created basic Nav, Home and Products components

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { directive } from '@babel/types';
+
+const Home = () => {
+    return(
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome Home</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/Products.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { directive } from '@babel/types';
+
+const Products = () => {
+    return(
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome to Products List!</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Products
