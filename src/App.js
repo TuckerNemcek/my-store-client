ad81eb2e7195b7ac8c9a9478938227414d0b075d
TuckerNemcek <tuckernemcek21@gmail.com>
2019-07-17 20:38:39 -0600

    when clicking on my Product card, my url becomes undefined

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navigation from './components/Navigation'
 import Home from './components/Home'
 import ProductsList from './components/ProductsList'
+import ProductDetail from './components/ProductDetail'
 
 
 function App() {
@@ -14,7 +15,8 @@ function App() {
             <Route path='/' component ={Navigation} />
             <Switch>
                 <Route exact path='/' component={Home} />
-                <Route  path='/products' component={ProductsList} />
+                <Route  exact path='/products' component={ProductsList} />
+                <Route  path ='/products/:id' component ={ProductDetail}/>
 
             </Switch>
         </Router>
