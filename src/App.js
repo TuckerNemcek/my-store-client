4988c24b468e117966bca65ac2991513da1e9d45
TuckerNemcek <tuckernemcek21@gmail.com>
2019-08-06 18:22:09 -0600

    added a payment success screen

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navigation from './components/Navigation'
 import Home from './components/Home'
 import ProductsList from './components/ProductsList'
 import ProductDetail from './components/ProductDetail'
+import PaymentSuccess from './components/PaymentSuccess'
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
                 <Route exact path='/' component={Home} />
                 <Route  exact path='/products' component={ProductsList} />
                 <Route  path ='/products/:id' component={ProductDetail} />
+                <Route  path ='/success' component={PaymentSuccess} />
             </Switch>
         </Router>
     )
